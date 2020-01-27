e96f4ef7938f70ac7a855066492693d800d82512
kiyotan815 <kyt.ibaragi0815@gmail.com>
2020-01-27 22:55:36 +0900

    ちょこっとしゅうせい

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="title f-green">Success fully deploy!!!!</h1>
+        <p className="f-green">
+          happpppppy hacccccccking!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/kiyotan815"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          welcome my github page
         </a>
       </header>
     </div>
